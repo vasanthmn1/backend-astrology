fc7dcda2b77d8e12ff712de24b2572e0f894a9f3
vasanthmn1 <mnvasanth4@gmail.com>
2023-05-14 20:28:38 +0530

    change

--- controller/applyAstrologyctrl.js
+++ controller/applyAstrologyctrl.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs');
 const userModel = require('../model/userModel');
 const jsonwebtoken = require('jsonwebtoken');
 const adminModel = require('../model/adminModel');
+const { response } = require('express');
 
 const applyAstro = asyncCntrol(async (req, res) => {
     try {
@@ -16,8 +17,12 @@ const applyAstro = asyncCntrol(async (req, res) => {
             type: "apply-doctor-request",
             message: `${newApplay.name} He Applayed  `,
             data: {
-                adminId: newApplay._id,
+                userId: newApplay._id,
                 name: newApplay.name,
+                email: newApplay.email,
+                address: newApplay.address,
+                times: newApplay.times,
+                date: newApplay.date,
                 onclickPath: '/user/apply'
             }
         })
@@ -25,7 +30,8 @@ const applyAstro = asyncCntrol(async (req, res) => {
             notifaction
         })
         res.status(200).json({
-            message: "apply Suceess"
+            message: "apply Suceess",
+
         })
         // await admin.findByIdAndUpdate({
         //     // admin._id,
@@ -50,12 +56,12 @@ const getAllNotification = asyncCntrol(async (req, res) => {
         const notification = user.notifaction
         seenotifi.push(...notification)
         user.notifaction = []
-        user.seenotnotifaction = notification
+        user.seenotnotifaction = seenotifi
 
         const updateuser = await user.save()
 
         res.status(200).json({
-            message: sucess,
+
             message: "all notification read",
             data: updateuser
         })
@@ -64,12 +70,39 @@ const getAllNotification = asyncCntrol(async (req, res) => {
     }
 })
 
+const delAllNotification = asyncCntrol(async (req, res) => {
+    try {
+        const user = await userModel.findOne({ _id: req.body.userId })
+        // const seenotifi = user.seenotnotifaction
+        // const notification = user.notifaction
+        if (user) {
+            user.notifaction = []
+            user.seenotnotifaction = []
+            const updateUser = await user.save()
+
+            updateUser.password = undefined
+
+            res.status(200).json({
+                message: "All notifications deleted",
+                data: updateUser
+            })
+        } else {
+            res.status(404).json({
+                message: "User not found",
+                data: null
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
 
 
 module.exports = {
     applyAstro,
-    getAllNotification
+    getAllNotification,
+    delAllNotification
 
 }
--- routes/applayUserRoute.js
+++ routes/applayUserRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { applyAstro, getAllNotification } = require('../controller/applyAstrologyctrl')
+const { applyAstro, getAllNotification, delAllNotification } = require('../controller/applyAstrologyctrl')
 // const { registerUser, loginUser } = require('../controller/userCtrl')
 
 const routes = express.Router()
@@ -7,6 +7,8 @@ const routes = express.Router()
 
 routes.post('/apply', applyAstro)
 routes.post('/readnoti', getAllNotification)
+routes.post('/deleteallnoti', delAllNotification)
+
 
 
 module.exports = routes
