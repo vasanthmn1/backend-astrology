c8acdd7fcc7d1432bf55e30530d428cc96856cff
vasanthmn1 <mnvasanth4@gmail.com>
2023-06-05 13:19:20 +0530

    👍

--- controller/zodiaCtrl.js
+++ controller/zodiaCtrl.js
@@ -21,7 +21,41 @@ const getpost = asyncCntrol(async (req, res) => {
         throw new Error(error)
     }
 })
+
+const delpost = asyncCntrol(async (req, res) => {
+    try {
+        const delPost = await zodiacModel.findByIdAndRemove(req.params.id,
+            req.body
+            , { new: true })
+        res.status(200).json(delPost)
+    } catch (error) {
+        res.status(400)
+        throw new Error(error)
+    }
+})
+
+const editpost = async (req, res) => {
+    try {
+        const editPost = await zodiacModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json(editPost)
+    } catch (error) {
+        res.status(400)
+        throw new Error(error)
+    }
+}
+const getone = asyncCntrol(async (req, res) => {
+    try {
+        const getPost = await zodiacModel.findById(req.params.id)
+        res.status(200).json(getPost)
+    } catch (error) {
+        res.status(400)
+        throw new Error(error)
+    }
+})
 module.exports = {
     createpost,
-    getpost
+    getpost,
+    delpost,
+    editpost,
+    getone
 }
--- routes/zodiacRoutes.js
+++ routes/zodiacRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const { createpost, getpost } = require('../controller/zodiaCtrl')
+const { createpost, getpost, delpost, editpost, getone } = require('../controller/zodiaCtrl')
 // const { registerUser, loginUser, getAdmin, getAllUser } = require('../controller/userCtrl')
 
 const routes = express.Router()
 routes.post('/create', createpost)
 routes.get('/get', getpost)
+routes.get('/get/:id', getone)
+
+routes.delete('/delete/:id', delpost)
+routes.put('/edit/:id', editpost)
 
 
 
